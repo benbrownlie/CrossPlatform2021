9a826f46b9e6bd403f7e14154ac9fb2fd7a49a21
Benjamin Brownlie <benjibspam@gmail.com>
2021-04-21 12:57:23 -0500

    Maze and EnemyMovement
    Added layout for maze. Began working on EnemyMovementBehavior. The enemy should be able to seek the player.

---
+++ Assets/Scripts/EnemyMovementBehavior.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovementBehavior : MonoBehaviour
+{
+    //Reference to the enemy's target
+    public GameObject target;
+    //Variable used to store and adjust the enemy's speed
+    public float speed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Sets the enemy's lookat function to be the passed in target
+        transform.LookAt(target.transform);
+
+        Vector3 direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        direction = (target.transform.position + target.transform.position - transform.position).normalized;
+        Vector3 desiredVelocity = direction * speed;
+
+    }
+}
