91d5ebfcff0dc41fe2282229b0f37871323422ba
Benjamin Brownlie <benjibspam@gmail.com>
2021-04-21 10:55:48 -0500

    Added player movement
    Ended up going with Van's player movement script. Players move through the scene using only the mouse.

--- Assets/Scripts/PlayerMovementBehavior.cs
+++ Assets/Scripts/PlayerMovementBehavior.cs
@@ -6,19 +6,17 @@ public class PlayerMovementBehavior : MonoBehaviour
 {
     //Variable used to store and adjust the player's movement speed
     public float speed;
-
     //Variable used to store the player's mouse position
     private Vector3 _mousePos;
-
-    //Reference to the player
-    private GameObject _playerRef;
-
-    private Ray _rayCast;
+    //Ray variable used to represent a ray to the mouse
+    private Ray _mouseRay;
+    //Reference to the rigidbody
+    public CharacterController characterController;
 
     // Start is called before the first frame update
     void Start()
     {
-        _playerRef = GetComponent<GameObject>();
+
     }
 
     // Update is called once per frame
@@ -32,7 +30,21 @@ public class PlayerMovementBehavior : MonoBehaviour
         {
             //Sets the _mousePos variable to be the mouse position
             _mousePos = Input.mousePosition;
-
+            //Sets the _mouseRay variable to the main camera and calls the ScreenPointToRay function with _mousePos passed in
+            _mouseRay = Camera.main.ScreenPointToRay(_mousePos);
+            //Create a plane at the player's position
+            Plane playerPlane = new Plane(Vector3.up, transform.position);
+            //Find how far from the camera the ray intersects the plane;
+            float rayDistance = 0.0f;
+            playerPlane.Raycast(_mouseRay, out rayDistance);
+            //Sets targetpoint to be the ray distance to the plane
+            Vector3 targetpoint = _mouseRay.GetPoint(rayDistance);
+            //Get the direction
+            direction = (targetpoint - transform.position).normalized;
         }
+        //Velocity variable is set to the direction scaled by the player's speed
+        Vector3 velocity = direction * speed;
+        //Moves the rigidbody to position
+        characterController.SimpleMove(velocity);
     }
 }
