506d68494041e34af74f814bdf80413c8f1d31f7
benbrownlie <benjibspam@gmail.com>
2021-04-22 21:08:43 -0500

    Added Restart and Quit button
    Added a Restart and Quit button that appear after the player dies

--- Assets/Scripts/GameManagerBehavior.cs
+++ Assets/Scripts/GameManagerBehavior.cs
@@ -1,9 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public delegate void GameEvent();
 
 public class GameManagerBehavior : MonoBehaviour
 {
+    //Bool to tell wether the game should end or not
+    private static bool _gameOver = false;
+    public static GameEvent OnGameOver;
+
+    //Reference to the player's health
+    public HealthBehavior _playerHealth;
+    //Reference to the gameover overlay
+    public GameObject _gameOverScreen;
+
+    public static bool GameOver
+    {
+        get
+        {
+            return _gameOver;
+        }
+    }
+
+    public void RestartGame()
+    {
+        //Returns to how the scene was at start
+        SceneManager.LoadScene(0);
+    }
+
+    public void EndGame()
+    {
+        //Closes the application
+        Application.Quit();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +45,9 @@ public class GameManagerBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //Sets game over to be the player's health less than or equal to 0
+        _gameOver = _playerHealth.Health <= 0;
+        //Sets the gameOver overlay to be visible
+        _gameOverScreen.SetActive(_gameOver);
     }
 }
